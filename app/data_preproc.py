246a69aa7b33248a31eb867c0d5b867fbf58ba84
LarsBreum <lars.breum.lh@gmail.com>
2025-01-27 15:33:01 +0100

    works for own dataset

--- app/data_preproc.py
+++ app/data_preproc.py
@@ -2,108 +2,42 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
-udp = pd.read_csv('./data/DrDoS_UDP.csv')
+udp = pd.read_csv('./data/attack.csv')
 udp_cols = list(udp.columns.values)
 
-ssdp = pd.read_csv('./data/DrDoS_SSDP.csv')
-ssdp_cols = list(ssdp.columns.values)
+benign = pd.read_csv('./data/benign.csv')
+benign_cols = list(benign.columns.values)
 
-snmp = pd.read_csv('./data/DrDoS_SNMP.csv')
-snmp_cols = list(snmp.columns.values)
 
-ntp = pd.read_csv('./data/DrDoS_NTP.csv')
-ntp_cols = list(ntp.columns.values)
-
-bios = pd.read_csv('./data/DrDoS_NetBIOS.csv')
-bios_cols = list(bios.columns.values)
-
-mssql=pd.read_csv('./data/DrDoS_MSSQL.csv')
-mssql_cols = list(mssql.columns.values)
-
-ldap=pd.read_csv('./data/DrDoS_LDAP.csv')
-ldap_cols = list(ldap.columns.values)
-
-dns=pd.read_csv('./data/DrDoS_DNS.csv')
-dns_cols = list(dns.columns.values)
-
-syn = pd.read_csv('./data/Syn.csv')
-syn_cols = list(syn.columns.values)
-syn2 = pd.read_csv('./data/Syn-2.csv')
-syn2_cols = list(syn.columns.values)
-
-portmap = pd.read_csv('./data/Portmap.csv')
-portmap_cols = list(portmap.columns.values)
-portmap2 = pd.read_csv('./data/Portmap-2.csv')
-
-udpLag = pd.read_csv('./data/UDPLag.csv')
-udpLag2 = pd.read_csv('./data/UDPLag-2.csv')
-udpLag_cols = list(udpLag.columns.values)
-
-udp = pd.read_csv('./data/UDP.csv')
-udp_cols = list(udp.columns.values)
-udp2 = pd.read_csv('./data/UDP-2.csv')
 
 print("---- SHAPES ----")
 
-min = min(udpLag.shape,udp.shape,syn.shape,bios.shape,mssql.shape,ldap.shape,portmap2.shape,syn2.shape,portmap2.shape,udp2.shape, udpLag2.shape)
+min = min(udp.shape, benign.shape)
 print("min =", min)
 
 
 print("----- 7 CLASSES -----")
-ld = ldap.iloc[:min[0],]
-ld.to_csv('./7_classes/ldap7.csv')
-f = portmap.iloc[:min[0],]
-f.to_csv('./7_classes/portmap7.csv')
-a = mssql.iloc[:min[0],]
-a.to_csv('./7_classes/mssql7.csv')
-b = bios.iloc[:min[0],]
-b.to_csv('./7_classes/bios7.csv')
-c = syn.iloc[:min[0],]
-c.to_csv('./7_classes/syn7.csv')
-d = udp.iloc[:min[0],]
-d.to_csv('./7_classes/udp7.csv')
 
-e = udpLag.iloc[:min[0],]
-e.to_csv('./7_classes/udpLag7.csv')
+a = udp.iloc[:min[0],]
+a.to_csv('./7_classes/attack.csv')
+b = benign.iloc[:min[0],]
+b.to_csv('./7_classes/benign.csv')
 
-f = syn2.iloc[:min[0],]
-f.to_csv('./7_classes/syn2.csv')
-
-g = portmap2.iloc[:min[0],]
-g.to_csv('./7_classes/portmap2.csv')
-
-h = udpLag2.iloc[:min[0],]
-h.to_csv('./7_classes/udpLag2.csv')
-
-i = udp2.iloc[:min[0],]
-i.to_csv('./7_classes/udp2.csv')
 
 # Working with 7 classes
 print("----- Reading 7_classes -----")
-ldap = pd.read_csv("./7_classes/ldap7.csv")
-mssql = pd.read_csv("./7_classes/mssql7.csv")
-bios = pd.read_csv("./7_classes/bios7.csv")
-portmap = pd.read_csv("./7_classes/portmap7.csv")
-prtmap2 = pd.read_csv("./7_classes/portmap2.csv")
-syn = pd.read_csv("./7_classes/syn7.csv")
-syn2 = pd.read_csv("./7_classes/syn2.csv")
-udp = pd.read_csv("./7_classes/udp7.csv")
-udp2 = pd.read_csv("./7_classes/udp2.csv")
-udplag = pd.read_csv("./7_classes/udpLag7.csv")
-udplag2 = pd.read_csv("./7_classes/udpLag2.csv")
+udp = pd.read_csv("./7_classes/attack.csv")
+benign = pd.read_csv("./7_classes/benign.csv")
+
 
 #checking for missing values
-ldap.isnull().sum()
-mssql.isnull().sum()
-bios.isnull().sum()
-portmap.isnull().sum()
-syn.isnull().sum()
 udp.isnull().sum()
-udplag.isnull().sum()
+benign.isnull().sum()
 
-print(ldap.describe())
 
-ds = pd.concat([ldap, mssql, bios, portmap, syn, udp, udplag, portmap2, syn2, udp2, udplag2])
+print(udp.describe())
+
+ds = pd.concat([udp, benign])
 
 ds.to_csv('./out_data/complete_dataset.csv')
 
@@ -120,42 +54,28 @@ print("df.describe()")
 print(df.describe())
 
 #removing columns where variation(std) is 0
-df=df.drop(' Bwd PSH Flags',axis=1)
-df=df.drop(' Fwd URG Flags',axis=1)
-df=df.drop(' Bwd URG Flags',axis=1)
+df=df.drop('Bwd PSH Flags',axis=1)
+df=df.drop('Fwd URG Flags',axis=1)
+df=df.drop('Bwd URG Flags',axis=1)
 df=df.drop('FIN Flag Count',axis=1)
-df=df.drop(' PSH Flag Count',axis=1)
-df=df.drop(' ECE Flag Count',axis=1)
-df=df.drop('Fwd Avg Bytes/Bulk',axis=1)
-df=df.drop(' Fwd Avg Packets/Bulk',axis=1)
-df=df.drop(' Fwd Avg Bulk Rate',axis=1)
-df=df.drop(' Bwd Avg Bytes/Bulk',axis=1)
-df=df.drop(' Bwd Avg Packets/Bulk',axis=1)
-df=df.drop('Bwd Avg Bulk Rate',axis=1)
+df=df.drop('PSH Flag Count',axis=1)
+df=df.drop('ECE Flag Count',axis=1)
+df=df.drop('Fwd Bytes/Bulk Avg',axis=1)
+df=df.drop('Fwd Packet/Bulk Avg',axis=1)
+df=df.drop('Fwd Bulk Rate Avg',axis=1)
+#df=df.drop('Bwd Bytes/Bulk Avg',axis=1)
+df=df.drop('Bwd Packet/Bulk Avg',axis=1)
+df=df.drop('Bwd Bulk Rate Avg',axis=1)
 df=df.drop('Unnamed: 0',axis=1)
 df=df.drop('Unnamed: 0.1',axis=1)
-df=df.drop('SimillarHTTP', axis=1)
+#df=df.drop('SimillarHTTP', axis=1)
 df=df.drop('Flow ID', axis=1)
 #df=df.drop(' Timestamp', axis=1)
 
 
-df['Label'] = df[' Label'].replace('BENIGN', '0')
-df['Label'] = df[' Label'].replace('NetBIOS', '1')
-df['Label'] = df[' Label'].replace('LDAP', '2')
-df['Label'] = df[' Label'].replace('MSSQL', '3')
-df['Label'] = df[' Label'].replace('Portmap', '4')
-df['Label'] = df[' Label'].replace('Syn', '5')
-df['Label'] = df[' Label'].replace('UDP', '6')
-df['Label'] = df[' Label'].replace('UDPLag', '7')
+df['Label'] = df['Label'].replace('benign', '0')
+df['Label'] = df['Label'].replace('attack', '1')
 
-df['Label'] = df[' Label'].replace('BENIGN', '0')
-df['Label'] = df[' Label'].replace('DrDos_NetBIOS', '1')
-df['Label'] = df[' Label'].replace('DrDoS_LDAP ', '2')
-df['Label'] = df[' Label'].replace('DrDos_MSSQL', '3')
-df['Label'] = df[' Label'].replace('Portmap', '4')
-df['Label'] = df[' Label'].replace('Syn', '5')
-df['Label'] = df[' Label'].replace('UDP', '6')
-df['Label'] = df[' Label'].replace('UDPLag', '7')
 
 #df['Label'] = df[' Label'].astype('int')
 print(df.Label.unique())
