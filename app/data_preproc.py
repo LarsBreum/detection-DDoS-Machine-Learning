48a817d210f10cedd53c97e92413c179ebf04984
LarsBreum <lars.breum.lh@gmail.com>
2025-01-29 10:21:18 +0100

    ip and timestamp works

--- app/data_preproc.py
+++ app/data_preproc.py
@@ -63,18 +63,18 @@ df=df.drop('ECE Flag Count',axis=1)
 df=df.drop('Fwd Bytes/Bulk Avg',axis=1)
 df=df.drop('Fwd Packet/Bulk Avg',axis=1)
 df=df.drop('Fwd Bulk Rate Avg',axis=1)
-#df=df.drop('Bwd Bytes/Bulk Avg',axis=1)
+df=df.drop('Bwd Bytes/Bulk Avg',axis=1)
 df=df.drop('Bwd Packet/Bulk Avg',axis=1)
 df=df.drop('Bwd Bulk Rate Avg',axis=1)
 df=df.drop('Unnamed: 0',axis=1)
 df=df.drop('Unnamed: 0.1',axis=1)
 #df=df.drop('SimillarHTTP', axis=1)
 df=df.drop('Flow ID', axis=1)
-#df=df.drop(' Timestamp', axis=1)
+#df=df.drop('Timestamp', axis=1)
 
 
 df['Label'] = df['Label'].replace('benign', '0')
-df['Label'] = df['Label'].replace('attack', '1')
+df['Label'] = df['Label'].replace('UDP', '1')
 
 
 #df['Label'] = df[' Label'].astype('int')
@@ -83,61 +83,70 @@ print(df.Label.unique())
 print("Print label value counts:")
 print(df.Label.value_counts())
 
-df.to_csv('./out_data/complete_dataset.csv')
 
-print('num = ',len(df[' Source IP'].unique()))
-df[' Source IP'].unique()
+print(df.count())
+
+print('num = ',len(df['Src IP'].unique()))
+df['Src IP'].unique()
 le = LabelEncoder()
 a = df
 
 
 import netaddr
 print(int(netaddr.IPAddress('192.168.4.54')))
-ips = df[' Destination IP']
+ips = df['Dst IP']
 converted=[]
 for i in range(len(ips)):
   converted.append(int(netaddr.IPAddress(ips[i])))
 print("done")
 
 #replacing ip in df with its decimal form
-ips = df[' Source IP'].unique()
+ips = df['Src IP'].unique()
 l = len(ips)
 print('starting loop, length is',l)
 
 for i in range(l):
-  df[' Source IP'] = df[' Source IP'].replace(ips[i], int(netaddr.IPAddress(ips[i])))
+  df['Src IP'] = df['Src IP'].replace(ips[i], int(netaddr.IPAddress(ips[i])))
 print('loop over')
 
 df['Flow Bytes/s'] = df['Flow Bytes/s'].astype('float')
 
-print("---- HANDLING TIMESTAMPS -----")
-print("len timestamps:" + str(len(df[' Timestamp'])))
+
+
+print("---- HANDLING Timestamps -----")
+print("len Timestamps:" + str(len(df['Timestamp'])))
 
 y=[]
 m=[]
 d=[]
 h=[]
 mi=[]
-for i in range(len(df[' Timestamp'])):
-  a=int(df[' Timestamp'][0][0:4])
+for i in range(len(df['Timestamp'])):
+  a=int(df['Timestamp'][0][6:10])
   y.append(a)
 
-  b= int(df[' Timestamp'][0][5:7])
+  b= int(df['Timestamp'][0][3:5])
   m.append(b)
 
-  c = int(df[' Timestamp'][0][8:10])
+  c = int(df['Timestamp'][0][0:2])
   d.append(c)
 
-  e= int(df[' Timestamp'][0][11:13])
+  e= int(df['Timestamp'][0][11:13])
   h.append(e)
 
-  f= int(df[' Timestamp'][0][14:16])
+  f= int(df['Timestamp'][0][14:16])
   mi.append(f)
 
   if(i%1000==0):
     print('completed ',i)
 
-print("---- mi -----")
-mi = pd.DataFrame(mi)
 
+df['year'] = y
+df['month'] = m
+df['day'] = d
+df['hour'] = h
+df['minute'] = mi
 
+df.drop(columns = ['Timestamp'])
+
+df.to_csv('./out_data/complete_dataset.csv')
