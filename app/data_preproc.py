4682b56dd36a983d2f5758cb3fc121dca1355ab9
LarsBreum <lars.breum.lh@gmail.com>
2025-01-29 19:44:32 +0100

    added benign dataset

--- app/data_preproc.py
+++ app/data_preproc.py
@@ -15,13 +15,13 @@ benign_df = pd.concat([benign_df, Portmap], ignore_index=True)
 print("concated portmap")
 del(Portmap)
 
-Syn = pd.read_csv('./data/Syn.csv', low_memory=False)
-print("loaded Syn")
-Syn = Portmap[Syn[' Label'] == 'BENIGN']
-print("kept only benign")
-benign_df = pd.concat([benign_df, Syn], ignore_index=True)
-print("concated Syn")
-del(Syn)
+# Syn = pd.read_csv('./data/Syn.csv', low_memory=False)
+# print("loaded Syn")
+# Syn = Portmap[Syn[' Label'] == 'BENIGN']
+# print("kept only benign")
+# benign_df = pd.concat([benign_df, Syn], ignore_index=True)
+# print("concated Syn")
+# del(Syn)
 
 print(benign_df.describe())
 
@@ -55,9 +55,11 @@ udp2.isnull().sum()
 
 udp[' Destination IP'] = udp[' Destination IP'].fillna(0)
 udp2[' Destination IP'] = udp2[' Destination IP'].fillna(0)
+benign_df[' Destination IP'] = benign_df[' Destination IP'].fillna(0)
 
 udp[' Source IP'] = udp[' Source IP'].fillna(0)
 udp2[' Source IP'] = udp2[' Source IP'].fillna(0)
+benign_df[' Source IP'] = benign_df[' Source IP'].fillna(0)
 
 df = pd.concat([udp, udp2])
 
@@ -91,17 +93,40 @@ df=df.drop('SimillarHTTP', axis=1)
 df=df.drop('Flow ID', axis=1)
 #df=df.drop(' Timestamp', axis=1)
 
+#removing columns where variation(std) is 0
+benign_df=benign_df.drop(' Bwd PSH Flags',axis=1)
+benign_df=benign_df.drop(' Fwd URG Flags',axis=1)
+benign_df=benign_df.drop(' Bwd URG Flags',axis=1)
+benign_df=benign_df.drop('FIN Flag Count',axis=1)
+benign_df=benign_df.drop(' PSH Flag Count',axis=1)
+benign_df=benign_df.drop(' ECE Flag Count',axis=1)
+benign_df=benign_df.drop('Fwd Avg Bytes/Bulk',axis=1)
+benign_df=benign_df.drop(' Fwd Avg Packets/Bulk',axis=1)
+benign_df=benign_df.drop(' Fwd Avg Bulk Rate',axis=1)
+benign_df=benign_df.drop(' Bwd Avg Bytes/Bulk',axis=1)
+benign_df=benign_df.drop(' Bwd Avg Packets/Bulk',axis=1)
+benign_df=benign_df.drop('Bwd Avg Bulk Rate',axis=1)
+benign_df=benign_df.drop('Unnamed: 0',axis=1)
+#dfbenign_=df.drop('Unnamed: 0.1',axis=1)
+benign_df=benign_df.drop('SimillarHTTP', axis=1)
+benign_df=benign_df.drop('Flow ID', axis=1)
+#df=df.drop(' Timestamp', axis=1)
+
 
 df['Label'] = df['Label'].replace('BENIGN', '0')
 df['Label'] = df['Label'].replace('UDP', '1')
 df['Label'] = df['Label'].replace('DrDoS_UDP', '1')
 
+benign_df['Label'] = benign_df[' Label'].replace('BENIGN', '0')
+
 #df['Label'] = df[' Label'].astype('int')
 print("UNIQUE LABELS: ")
 print(df.Label.unique())
+print(benign_df.Label.unique())
 
 print("Print label value counts:")
 print(df.Label.value_counts())
+print(benign_df.Label.value_counts())
 
 
 print('num = ',len(df[' Source IP'].unique()))
@@ -109,51 +134,54 @@ df[' Source IP'].unique()
 le = LabelEncoder()
 a = df
 
-
 import netaddr
-print(int(netaddr.IPAddress('192.168.4.54')))
-ips = df[' Destination IP']
-converted=[]
-for i in range(len(ips)):
-  try:
-    converted.append(int(netaddr.IPAddress(ips[i])))
 
-  except:
-    converted.append(0)
+def convert_ips(column_name, dataframe):
+  ips = dataframe[columne_name].unique()
+  l = len(ips)
+  print('starting loop, length is',l)
+  for i in range(l):
+    dataframe[column_name] = dataframe[column_name].replace(ips[i], int(netaddr.IPAddress(ips[i])))
+  print("loop over")
+  return dataframe
+  
 
-print("done")
 
 #replacing ip in df with its decimal form
-ips = df[' Source IP'].unique()
-l = len(ips)
-print('starting loop, length is',l)
+df = convert_ips(' Source IP', df)
+df = convert_ips(' Destination IP', df)
 
-for i in range(l):
-  df[' Source IP'] = df[' Source IP'].replace(ips[i], int(netaddr.IPAddress(ips[i])))
-print('loop over')
+benign_df = convert_ips(' Source IP', benign_df)
+benign_df = convert_ips(' Destination IP', benign_df)
 
 df['Flow Bytes/s'] = df['Flow Bytes/s'].astype('float')
+benign_df['Flow Bytes/s'] = benign_df['Flow Bytes/s'].astype('float')
+
 
 print("---- HANDLING TIMESTAMPS -----")
 print("len timestamps: " + str(len(df[' Timestamp'])))
 print(df.head())
 
 df['Timestamp'] = df[' Timestamp'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S.%f").timestamp())
+benign_df['Timestamp'] = benign_df[' Timestamp'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S.%f").timestamp())
 
 
 df=df.drop(' Timestamp',axis=1)
+benign_df=benign_df.drop(' Timestamp',axis=1)
 
 print("FINAL DF:")
 print("head")
 print(df.head())
 
-print("describe")
+
+print("describe df")
 print(df.describe())
+
 
 df.to_csv('./out_data/complete_dataset.csv')
 
 # Write only benign data set
 print("----- BENIGN -----")
-benign_df = df[df['Label'] == '0']
+benign_df = pd.concat([benign_df, df[df['Label'] == '0']], ignore_index=True)
 print(benign_df.describe())
 benign_df.to_csv('./out_data/benign_cicdos_dataset.csv')
